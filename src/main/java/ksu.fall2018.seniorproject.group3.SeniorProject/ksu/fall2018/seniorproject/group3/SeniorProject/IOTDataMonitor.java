879800d164f2d7e897b45aecfc9414cddbd572f2
Mark Zeagler <mark.zeagler@outlook.com>
2018-09-04 21:04:06 -0400

    Converted to Java 9 module

--- src/main/java/ksu.fall2018.seniorproject.group3.SeniorProject/ksu/fall2018/seniorproject/group3/SeniorProject/IOTDataMonitor.java
+++ src/main/java/ksu.fall2018.seniorproject.group3.SeniorProject/ksu/fall2018/seniorproject/group3/SeniorProject/IOTDataMonitor.java
@@ -1,7 +1,7 @@
 package ksu.fall2018.seniorproject.group3.SeniorProject;
 
 /**
- * @author Mark Zeagler
+ * @author Mark Zeagler, Aisha Siddiqui, Drashtee Parmar, Michael Bowman, Shamita Hattangady
  * @version 1.0
  */
 public class IOTDataMonitor {
---
+++ src/main/java/ksu.fall2018.seniorproject.group3.SeniorProject/module-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author Mark Zeagler, Aisha Siddiqui, Drashtee Parmar, Michael Bowman, Shamita Hattangady
+ * @version 1.0
+ */module SeniorProject {
+ 	exports ksu.fall2018.seniorproject.group3.SeniorProject;
+}
