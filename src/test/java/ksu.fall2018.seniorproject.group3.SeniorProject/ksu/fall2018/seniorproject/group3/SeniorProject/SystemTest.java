91cf49889b435f3fa970cc1caf1c8c969fa03c82
Mark Zeagler <mark.zeagler@outlook.com>
2018-09-13 16:47:25 -0400

    Added Kafka Producer to system test

--- src/test/java/ksu.fall2018.seniorproject.group3.SeniorProject/ksu/fall2018/seniorproject/group3/SeniorProject/SystemTest.java
+++ src/test/java/ksu.fall2018.seniorproject.group3.SeniorProject/ksu/fall2018/seniorproject/group3/SeniorProject/SystemTest.java
@@ -1,7 +1,16 @@
 package ksu.fall2018.seniorproject.group3.SeniorProject;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
@@ -14,12 +23,32 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
  * @version 1.0
  */
 class SystemTest {
+
+	private static KafkaProducer<Long, String> producer;
+	private final String TOPIC = "JUnit Test"; // TODO Change this once we have the name
+
+	@BeforeAll public static void setup() {
+		Properties properties = new Properties();
+		properties.put( ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092" );
+		properties.put( ProducerConfig.CLIENT_ID_CONFIG, "KafkaExampleProducer" );
+		properties.put( ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName() );
+		properties.put( ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName() );
+		producer = new KafkaProducer<>( properties );
+	}
+
 	// Test that the system can handle the required ~500 messages per second and twice the required ~500 messages per second
 	@ParameterizedTest @ValueSource( ints = { 500, 1000 } ) void testSystemBandwidth( int messagesToHandle ) {
 		// TODO Make sure all setup is done before here, so after the startTime is recorded, the system is fully functional
 		long startTime = System.currentTimeMillis();
 		for ( int i = 0; i < messagesToHandle; i++ ) {
-			// TODO Send the messages
+			ProducerRecord<Long, String> producerRecord = new ProducerRecord<>( TOPIC, "Message: " + i );
+			try {
+				producer.send( producerRecord );
+			} catch ( Exception e ) { // TODO Better way to abort and ignore when there's an exception?
+				e.printStackTrace();
+				assumeTrue( false,
+						"The test was aborted and ignored because an exception was thrown in the producer loop," );
+			}
 		}
 		long producerEndTime = System.currentTimeMillis();
 		// If the producer cannot generate the messages fast enough, we want to halt the test without failing
