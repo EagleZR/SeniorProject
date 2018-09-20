2f9b3c92257717a0267330d9b1067a62c3de1bd5
Mark Zeagler <mark.zeagler@outlook.com>
2018-09-20 16:27:49 -0400

    Connected test with Kafka server

--- src/test/java/ksu.fall2018.seniorproject.group3.SeniorProject/ksu/fall2018/seniorproject/group3/SeniorProject/SystemTest.java
+++ src/test/java/ksu.fall2018.seniorproject.group3.SeniorProject/ksu/fall2018/seniorproject/group3/SeniorProject/SystemTest.java
@@ -1,6 +1,7 @@
 package ksu.fall2018.seniorproject.group3.SeniorProject;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.LongSerializer;
@@ -26,39 +27,57 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
  * @author Mark Zeagler, Aisha Siddiqui, Drashtee Parmar, Michael Bowman, Shamita Hattangady
  * @version 1.0
  */
-class SystemTest {
+class SystemTest extends ConfigUser {
 
-	private static final ArrayList<String> SAMEPLE_MESSAGES = new ArrayList<>();
-	private static KafkaProducer<Long, String> producer;
+	// http://kafka.apache.org/quickstart
+
+	private static final ArrayList<String> SAMPLE_MESSAGES = new ArrayList<>();
+	private static Producer<Long, String> producer;
 
 	static {
 		try {
 			File file = new File( "resources/sample_messages.txt" );
-			Files.lines( file.toPath() ).forEach( SAMEPLE_MESSAGES::add );
+			Files.lines( file.toPath() ).forEach( SAMPLE_MESSAGES::add );
 		} catch ( IOException e ) {
 			fail( "The sample messages were not loaded successfully." );
 		}
 	}
 
-	private final String TOPIC = "JUnit Test"; // TODO Change this once we have the name
+	private final String TOPIC;
+
+	SystemTest() {
+		TOPIC = getEnvIni().get( "Settings", "topic", String.class );
+	}
 
 	@BeforeAll static void setup() {
 		Properties properties = new Properties();
-		properties.put( ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092" );
-		properties.put( ProducerConfig.CLIENT_ID_CONFIG, "KafkaExampleProducer" );
+		//		properties
+		//				.put( ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, getEnvIni().get( "Settings", "servers", String.class ) );
+		System.out.println(
+				"Opening producer on servers: \"" + getEnvIni().get( "Settings", "servers", String.class ) + "\"" );
+		properties
+				.put( ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, getEnvIni().get( "Settings", "servers", String.class ) );
+		properties.put( ProducerConfig.CLIENT_ID_CONFIG, "KafkaJUnitProducer" );
 		properties.put( ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName() );
 		properties.put( ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName() );
 		producer = new KafkaProducer<>( properties );
 	}
 
-	// Test that the system can handle the required ~500 messages per second and twice the required ~500 messages per second
+	/**
+	 * Test that the system can handle the required ~500 messages per second and twice the required ~500 messages per
+	 * second
+	 *
+	 * @param messagesToHandle The number of messages that we're trying to send/receive in a single second.
+	 */
 	@ParameterizedTest @ValueSource( ints = { 500, 1000 } ) void testSystemBandwidth( int messagesToHandle ) {
 		// TODO Make sure all setup is done before here, so after the startTime is recorded, the system is fully functional
+		System.out.println( "Starting " + 500 + " message test on topic " + TOPIC );
 		long startTime = System.currentTimeMillis();
 		for ( int i = 0; i < messagesToHandle; i++ ) {
 			ProducerRecord<Long, String> producerRecord = new ProducerRecord<>( TOPIC,
-					SAMEPLE_MESSAGES.get( i % SAMEPLE_MESSAGES.size() ) );
+					SAMPLE_MESSAGES.get( i % SAMPLE_MESSAGES.size() ) );
 			try {
+				System.out.println( "Sending message: " + SAMPLE_MESSAGES.get( i % SAMPLE_MESSAGES.size() ) );
 				producer.send( producerRecord );
 			} catch ( Exception e ) { // TODO Better way to abort and ignore when there's an exception?
 				e.printStackTrace();
