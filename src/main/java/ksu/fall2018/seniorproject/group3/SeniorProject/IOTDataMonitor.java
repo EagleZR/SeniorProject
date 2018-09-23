d5d3fa888b81aa89305b2941302a86a966fe71c7
Mark Zeagler <mark.zeagler@outlook.com>
2018-09-23 15:14:29 -0400

    Refactored Directories

--- src/main/java/ksu/fall2018/seniorproject/group3/SeniorProject/IOTDataMonitor.java
+++ src/main/java/ksu/fall2018/seniorproject/group3/SeniorProject/IOTDataMonitor.java
@@ -4,9 +4,10 @@ package ksu.fall2018.seniorproject.group3.SeniorProject;
  * @author Mark Zeagler, Aisha Siddiqui, Drashtee Parmar, Michael Bowman, Shamita Hattangady
  * @version 1.0
  */
-public class IOTDataMonitor {
+public class IOTDataMonitor extends ConfigUser {
 
 	public static void main( String[] args ) {
 
 	}
+
 }
